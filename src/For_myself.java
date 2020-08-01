38a5f1806dc5c8cdaac390876efec04aa9fdd137
Fatma66 <baghirovafatma@gmail.com>
2020-08-01 12:07:09 -0400

    add For_mysefl.java

---
+++ src/For_myself.java
@@ -0,0 +1,6 @@
+public class For_myself {
+    public static void main(String[] args) {
+        System.out.println("I am new here");
+        //it is just for me
+    }
+}
--- src/HappyB20.java
+++ src/HappyB20.java
@@ -1,6 +1,9 @@
 public class HappyB20 {
     public static void main(String[] args) {
         System.out.println("happy August");
+
+        //brand new line is add
+
     }
 
 }
